7cc8dfd3682c0cda563d71438b5ce2b855941fbe
Arnaud Renaud <arnaud.renaud@gmail.com>
2023-05-04 10:48:27 +0200

    Show note titles in side bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [notes, setNotes] = useState(null);
+
+  const fetchNotes = async () => {
+    const response = await fetch("/notes");
+    const result = await response.json();
+    setNotes(result);
+  };
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
   return (
     <>
-      <aside className="Side"></aside>
+      <aside className="Side">
+        {notes &&
+          notes.map((note) => <div className="Note-title">{note.title}</div>)}
+      </aside>
       <main className="Main"></main>
     </>
   );
