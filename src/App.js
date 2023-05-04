5f1a229b41f08eb6e4cb40d03135cce2bb969c11
sSh4rK <sharkytb25@gmail.com>
2023-05-04 18:04:49 +0200

    yo

--- src/App.js
+++ src/App.js
@@ -5,35 +5,62 @@ import "./App.css";
 import Note from "./components/Note/Note";
 
 function App() {
-  const [notes, setNotes] = useState(null);
+    const [notes, setNotes] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
 
-  const fetchNotes = async () => {
-    const response = await fetch("/notes");
-    const result = await response.json();
-    setNotes(result);
-  };
+    const fetchNotes = async () => {
+        const response = await fetch("/notes");
+        const result = await response.json();
+        setNotes(result);
+        setIsLoading(false);
+    };
 
-  useEffect(() => {
-    fetchNotes();
-  }, []);
+    useEffect(() => {
+        fetchNotes();
+    }, []);
 
-  return (
-    <>
-      <aside className="Side">
-        {notes &&
-          notes.map((note) => (
-            <Link to={`/notes/${note.id}`} className="Note-link">
-              {note.title}
-            </Link>
-          ))}
-      </aside>
-      <main className="Main">
-        <Routes>
-          <Route path="/notes/:id" Component={Note} />
-        </Routes>
-      </main>
-    </>
-  );
+    const addNote = async () => {
+        const response = await fetch(`/notes`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ title: "", content: "" }),
+        });
+        const result = await response.json();
+        setNotes([...notes, result]);
+    };
+
+    return (
+        <>
+            <aside className="Side">
+                {isLoading ? (
+                    "Chargement..."
+                ) : notes ? (
+                    notes.map((note) => (
+                        <Link
+                            to={`/notes/${note.id}`}
+                            className="Note-link"
+                            key={note.id}
+                        >
+                            {note.title}
+                        </Link>
+                    ))
+                ) : (
+                    <div>Aucune note trouvée</div>
+                )}
+                <button onClick={addNote}>Ajouter une note</button>
+            </aside>
+            <main className="Main">
+                <Routes>
+                    <Route
+                        path="/notes/:id"
+                        element={<Note OnSubmit={fetchNotes} />}
+                    />
+                </Routes>
+            </main>
+        </>
+    );
 }
 
 export default App;
--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -3,42 +3,71 @@ import { useParams } from "react-router";
 
 import "./Note.css";
 
-const Note = () => {
-  const { id } = useParams();
-  const [note, setNote] = useState(null);
+const Note = ({ OnSubmit }) => {
+    const { id } = useParams();
+    const [note, setNote] = useState(null);
 
-  const fetchNote = useCallback(async () => {
-    const response = await fetch(`/notes/${id}`);
-    const result = await response.json();
-    setNote(result);
-  }, [id]);
+    const fetchNote = useCallback(async () => {
+        const response = await fetch(`/notes/${id}`);
+        const result = await response.json();
+        setNote(result);
+    }, [id]);
 
-  useEffect(() => {
-    fetchNote();
-  }, [id, fetchNote]);
+    useEffect(() => {
+        fetchNote();
+    }, [id, fetchNote]);
 
-  return (
-    <form className="Form">
-      <input
-        className="Note-editable Note-title"
-        type="text"
-        value={note ? note.title : ""}
-        onChange={(event) => {
-          setNote({ ...note, title: event.target.value });
-        }}
-      />
-      <textarea
-        className="Note-editable Note-content"
-        value={note ? note.content : ""}
-        onChange={(event) => {
-          setNote({ ...note, content: event.target.value });
-        }}
-      />
-      <div className="Note-actions ">
-        <button className="Button">Enregistrer</button>
-      </div>
-    </form>
-  );
+    const updateNote = async (event) => {
+        event.preventDefault();
+        await fetch(`/notes/${id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(note),
+        });
+        OnSubmit();
+    };
+
+    const deleteNote = async (event) => {
+        event.preventDefault();
+        await fetch(`/notes/${id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(note),
+        });
+        OnSubmit();
+    };
+
+    return (
+        <form className="Form">
+            <input
+                className="Note-editable Note-title"
+                type="text"
+                value={note ? note.title : ""}
+                onChange={(event) => {
+                    setNote({ ...note, title: event.target.value });
+                }}
+            />
+            <textarea
+                className="Note-editable Note-content"
+                value={note ? note.content : ""}
+                onChange={(event) => {
+                    setNote({ ...note, content: event.target.value });
+                }}
+            />
+            <div className="Note-actions ">
+                <button className="Button" onClick={updateNote}>
+                    Enregistrer
+                </button>
+                <button className="Button" onClick={deleteNote}>
+                    Supprimer
+                </button>
+            </div>
+        </form>
+    );
 };
 
 export default Note;
